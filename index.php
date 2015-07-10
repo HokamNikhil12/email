68ca97cb80edcbbdb7d2da94babf6832b6a9225e
esense53 <nikhil.hokam@yahoo.in>
2015-07-10 16:24:30 +0530

    <Message>

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<title>Email</title>
+	<head></head>
+	<body>
+		<form action="email.php" method="post">
+			<div style="float:left">Username : </div><div><input type="text" name="uname" /><br /></div>
+			<div style="float:left; margin-top:10px;">Password : </div><div><input type="password" style="margin-top:10px;" name="upass" /><br /></div>
+
+			<div style="margin-top:15px; margin-left:70px;"><input type="submit" name="submit" value="submit"></div>
+
+		</form>
+	</body>
+</html>
